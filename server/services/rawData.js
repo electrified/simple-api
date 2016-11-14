b9afd08bf7ffd377814fb9912a4c5b6134b8d133
Graham Neilson <graham.c.neilson@gmail.com>
2016-11-14 20:24:13 +0000

    Generate better dates and times

--- server/services/rawData.js
+++ server/services/rawData.js
@@ -24,33 +24,61 @@ const competitions = [
  * Generate events
  */
 let id = 1;
-let daysToAdd = 0;
 let events = [];
 
+const today = new Date().getDay();
+
+// football is on tues, weds, sat & sun, say
+// cricket thursday to monday
+// tennis weekdays
+const football = [0,2,3,6];
+const tennis = [1,2,3,4,5];
+const cricket = [0,1,4,5,6];
+
 competitions.forEach((competition, competitionIndex) => { // each competition
-  // except League 1 & Queens. 
+
+  // except League 1 & Queens.
   if (competition.id === 3 || competition.id === 6) return;
 
+  let sportDates;
+  const sportId = competition.sportId;
+
+  if (sportId === 1) {
+    sportDates = [...football];
+  } else if (sportId === 2) {
+    sportDates = [...tennis];
+  } else {
+    sportDates = [...cricket];
+  }
+
+  let counter = 0;
   [1,2,3,4,5,6,7,8,9,10].forEach(eventIndex => { // has 10 events
+
     const date = new Date(); // on one of the seven days
+    const daysToAdd = sportDates[counter] < today ?
+      7 - (today - sportDates[counter]) : sportDates[counter] - today;
+
     date.setDate(date.getDate() + daysToAdd);
 
     events.push({
       id,
       title: `${competition.title} - Event ${eventIndex}`,
       competitionId: competition.id,
-      date: date, 
-      time: date,       
+      date: date.toLocaleDateString(),
+      time: date.toLocaleTimeString(),
     });
 
-    id++;
-    // skip 2 days from now in order to mock a disabled date.
-    (daysToAdd === 2) ?
-      daysToAdd = daysToAdd + 2 : daysToAdd++;
+    if (counter === sportDates.length - 1) {
+      counter = 0;
+    } else {
+      counter++;
+    }
 
-    daysToAdd === 7 && (daysToAdd = 0);
+    id++;
   });
 });
+
+console.log(events);
 
 /**
  * Generate markets - every event has 3 markets, say.
