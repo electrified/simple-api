531e2d2cc1df22360543c5584a4cdfe43a67cfd8
Graham Neilson <graham.c.neilson@gmail.com>
2018-06-16 11:53:57 +0100

    split account balance in response -balanc and bonus

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,11 +1,24 @@
 const accountRouter = require('express').Router()
 
 accountRouter.get('/account-details', (req, res) => {
-  res.status(200).json({
-    userId: 123,
-    name: 'Graham',
-    balance: 1000,
-  })
+	res.status(200).json({
+		userId: 123,
+		name: 'Graham',
+		balance: {
+			sportsbook: 985.00,
+			casino: 125.00,
+		},
+		bonus: {
+			sportsbook: 10.00,
+			casino: 50.00,
+		},
+	})
+})
+
+accountRouter.get('/login', (req, res) => {
+	res.status(200).json({
+		jwt: 'whatever',
+	})
 })
 
 module.exports = accountRouter
--- server/routes/catalog.js
+++ server/routes/catalog.js
@@ -2,28 +2,28 @@ const catalogRouter = require('express').Router()
 const { getEvents, getCompetitions } = require('../data')
 
 catalogRouter.get('/:sport/:competition/events', (req, res) => {
-  const sport = req.params.sport
-  const competition = req.params.competition
+	const sport = req.params.sport
+	const competition = req.params.competition
 
-  res.status(200).json({
-    message: 'Sport competition data for ....',
-    sport,
-    competition,
-  })
+	res.status(200).json({
+		message: 'Sport competition data for ....',
+		sport,
+		competition,
+	})
 
-  return
+	return
 })
 
 catalogRouter.get('/:sport/events', (req, res) => {
-  const sport = req.params.sport
-  res.status(200).json(getEvents(sport))
-  return
+	const sport = req.params.sport
+	res.status(200).json(getEvents(sport))
+	return
 })
 
 catalogRouter.get('/:sport/competitions', (req, res) => {
-  const sport = req.params.sport
-  res.status(200).json(getCompetitions(sport))
-  return
+	const sport = req.params.sport
+	res.status(200).json(getCompetitions(sport))
+	return
 })
 
 
