f88871605abddf91a3a0f93322a2ec962ba95fb6
Graham Neilson <graham.neilson@skybettingandgaming.com>
2018-07-25 10:25:10 +0100

    mvp socket.io

--- server/config.js
+++
@@ -1,6 +0,0 @@
-// Open shift or local settings
-const port = 1234
-
-module.exports = {
-	port,
-}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 const app = require('./app')
-const server = require('./config')
+const port = 1234
 
-app.listen(server.port)
-console.log('Server listening on port: ' + server.port)
+const server = require('http').createServer(app)
+var io = require('socket.io')(server)
+
+app.listen(port)
+
+io.on('connection', function (client) {
+  console.log('Client connected...')
+
+  client.on('join', function (data) {
+    console.log(data)
+  })
+
+})
+
+console.log('Server listening on port: ' + port)
