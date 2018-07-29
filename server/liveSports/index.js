27941d93881310082b93193cada7a67c9f37c2d8
Graham Neilson <graham.c.neilson@gmail.com>
2018-07-29 12:01:09 +0100

    simple outcome price update messages

--- server/liveSports/index.js
+++ server/liveSports/index.js
@@ -1,18 +1,41 @@
+const {flattenPageData} = require('../data/data.helpers')
+const data = require('../data/data')
 
-let interval
+const flattendData = flattenPageData(data.calcio.events)
+const outcomes = flattendData.outcomes
+const outcomesSize = outcomes.size
+
+let outcomeInterval
 
 module.exports = {
 	runLiveSports:
 		function (io, liveSportsConfig) {
 
-			console.log('liveSportsConfig')
-			console.log(liveSportsConfig)
-			clearInterval(interval)
+			const outcomeIntervalValue = liveSportsConfig && liveSportsConfig.outcome
 
-			interval = setInterval(function() {
-				io.emit('pushUpdate', {
-					number: 42,
-				})
-			}, 3000)
+			if (outcomeIntervalValue) {
+				clearInterval(outcomeInterval)
+
+				outcomeInterval = setInterval(function() {
+					io.emit('pushUpdate', getRandomOutcomeUpdate())
+				}, outcomeIntervalValue)
+			}
 		}
+}
+
+function getRandomOutcomeUpdate () {
+	const randomOutcomeIndex =
+		Math.floor(outcomesSize * Math.random())
+
+	// first item will be the outcome map key (id), second is Map entry value
+	const randomOutcome =
+		Array.from(outcomes)[randomOutcomeIndex][1]
+
+	const updatedOutcome = Object.assign(randomOutcome, {
+		price: (10 * Math.random()).toFixed(2),
+	})
+
+	return {
+		outcome: updatedOutcome
+	}
 }
