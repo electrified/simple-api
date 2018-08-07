eb08810de74c622ed91e2a6e23ecd654ec5b2eff
Ed Brindley <edward.brindley@skybettingandgaming.com>
2018-08-07 10:24:00 +0100

    make websockets pull from api

--- server/liveSports/index.js
+++ server/liveSports/index.js
@@ -1,9 +1,23 @@
 const {flattenPageData} = require('../data/data.helpers')
-const data = require('../data/data')
+const request =  require('request')
 
-const flattendData = flattenPageData(data.calcio.events)
-const outcomes = flattendData.outcomes
-const outcomesSize = outcomes.size
+let flattenedData = {}
+
+function getData(sport, callback) {
+	if(!flattenedData[sport]) {
+		
+		//http://localhost:8080/primary/${sport}/top/events
+		request(`https://vkyyqd7276.execute-api.eu-west-2.amazonaws.com/public/catalog/${sport}/events`, function (error, response, body) {
+			flattenedData[sport] = flattenPageData(JSON.parse(body))
+		
+			console.log(`got data for ${sport}`)
+			return callback(flattenedData[sport])
+		})
+	} else {
+		return callback(flattenedData[sport])
+	}
+}
+
 
 let outcomeInterval
 
@@ -12,30 +26,34 @@ module.exports = {
 		function (io, liveSportsConfig) {
 
 			const outcomeIntervalValue = liveSportsConfig && liveSportsConfig.outcome
+			const sport = liveSportsConfig && liveSportsConfig.sport || 'calcio'
 
 			if (outcomeIntervalValue) {
 				clearInterval(outcomeInterval)
 
 				outcomeInterval = setInterval(function() {
-					io.emit('pushUpdate', getRandomOutcomeUpdate())
+					console.log("emitting push update")
+					io.emit('pushUpdate', getRandomOutcomeUpdate(sport))
 				}, outcomeIntervalValue)
 			}
 		}
 }
 
-function getRandomOutcomeUpdate () {
-	const randomOutcomeIndex =
-		Math.floor(outcomesSize * Math.random())
+function getRandomOutcomeUpdate (sport) {
+	return getData(sport, function(sportData) {
+		const randomOutcomeIndex =
+		Math.floor(sportData.outcomes.size * Math.random())
 
-	// first item will be the outcome map key (id), second is Map entry value
-	const randomOutcome =
-		Array.from(outcomes)[randomOutcomeIndex][1]
+		// first item will be the outcome map key (id), second is Map entry value
+		const randomOutcome =
+			Array.from(sportData.outcomes)[randomOutcomeIndex][1]
 
-	const updatedOutcome = Object.assign(randomOutcome, {
-		price: (10 * Math.random()).toFixed(2),
+		const updatedOutcome = Object.assign(randomOutcome, {
+			price: (10 * Math.random()).toFixed(2),
+		})
+
+		return {
+			outcome: updatedOutcome
+		}
 	})
-
-	return {
-		outcome: updatedOutcome
-	}
 }
